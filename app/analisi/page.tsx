0fd575813b0b63743372baf48c11cc73355154e5
༺ɱαѵɠ૨เҡ༻ <mavgrik@icloud.com>
2025-05-17 20:32:53 +0200

    feat: aggiungi contenuto testuale e link di navigazione alla pagina di analisi climatica

--- app/analisi/page.tsx
+++ app/analisi/page.tsx
@@ -1,36 +1,61 @@
+import { CloudRain, Users, Waves } from 'lucide-react';
+import Link from 'next/link';
 import PortataPoChart from '@/components/charts/flow-rate';
 import PrecipitazioniChart from '@/components/charts/rainfall';
 import ConsumiIdriciChart from '@/components/charts/water-consumption';
+import { Separator } from '@/components/ui/separator';
 
-//TODO: Make the page
-
-/*
-  Grafico a torta:
-    Ripartizione settoriale dei consumi idrici (agricoltura vs industria vs civile) in Pianura Padana
-    se disponibili dati regionali ARPAE/ISPRA in formato CSV.
-      https://www.mdpi.com/2073-445X/11/1/35
-
-  Grafico ad area:
-    Portata del Po nel tempo
-
-  Grafico a barre:
-    Precipitazioni medie mensili negli anni
-*/
+//TODO: Add the text content to the charts
 
 export default function Analisi() {
   return (
-    <div className="space-y-6">
-      <div className="bg-muted rounded-lg border p-6">
-        <h2 className="mb-4">Precipitazioni medie mensili negli anni</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget nisi enim. Ut sodales urna vitae urna
-          mollis, non pretium justo sodales. Nulla ultricies, lorem quis aliquam eleifend, nunc leo dignissim orci, ut
-          faucibus ex elit efficitur mauris.
-        </p>
-        <div className="mb-4">
-          <PrecipitazioniChart />
+    <div className="flex flex-col items-start justify-start space-y-6">
+      <div className="ftl:flex-row ftl:space-x-6 ftl:space-y-0 flex w-full flex-col items-start justify-between space-y-6 space-x-0">
+        <div className="ftl:w-3/5 flex flex-col items-start justify-start">
+          <h1 className="mb-4 text-3xl">Analisi situazione climatica</h1>
+          <p className="text-muted-foreground">
+            Negli ultimi anni la Pianura Padana ha vissuto una grave siccità, con il Po ai minimi storici per scarse
+            piogge e alte temperature. Questo ha colpito agricoltura, acqua, energia e ambiente, evidenziando la
+            necessità di strategie di gestione sostenibile delle risorse idriche.
+          </p>
         </div>
-        <div className="mb-4">
+
+        <div className="ftl:w-2/5 bg-muted flex w-full flex-col items-start justify-between rounded-lg border p-3">
+          <h2 className="text-lg">Indice</h2>
+          <Separator className="mt-2 mb-4" />
+          <div className="flex w-full flex-row flex-wrap items-start justify-around space-y-2 space-x-6">
+            <Link
+              href="#precipitazioni"
+              className="hover:text-primary flex items-center text-sm transition-colors hover:underline"
+            >
+              <CloudRain className="mr-2 h-4 w-4" />
+              Precipitazioni mensili
+            </Link>
+            <Link
+              href="#portata"
+              className="hover:text-primary flex items-center text-sm transition-colors hover:underline"
+            >
+              <Waves className="mr-2 h-4 w-4" />
+              Portata del Po
+            </Link>
+            <Link
+              href="#utilizzi"
+              className="hover:text-primary flex items-center text-sm transition-colors hover:underline"
+            >
+              <Users className="mr-2 h-4 w-4" />
+              Utilizzi dell&apos;acqua
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div
+        id="precipitazioni"
+        className="bg-muted flex w-full flex-col items-start justify-start space-y-4 rounded-lg border p-6"
+      >
+        <h2 className="text-2xl">Precipitazioni medie mensili negli anni</h2>
+        <PrecipitazioniChart />
+        <div className="bg-card rounded-lg border p-4 transition-transform hover:scale-102">
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget nisi enim. Ut sodales urna vitae
             urna mollis, non pretium justo sodales. Nulla ultricies, lorem quis aliquam eleifend, nunc leo dignissim
@@ -45,13 +70,16 @@ export default function Analisi() {
         </div>
       </div>
 
-      <div className="ftl:flex-row ftl:space-x-6 ftl:space-y-0 flex w-full flex-col-reverse items-stretch space-y-6 space-y-reverse space-x-0">
-        <div className="bg-muted ftl:w-1/2 flex w-full items-center justify-center overflow-hidden rounded-lg border p-6">
+      <div
+        id="portata"
+        className="ftl:flex-row ftl:space-x-6 ftl:space-y-0 flex w-full flex-col-reverse items-stretch space-y-6 space-y-reverse space-x-0"
+      >
+        <div className="bg-muted ftl:w-2/5 flex w-full items-center justify-center rounded-lg border p-6">
           <PortataPoChart />
         </div>
 
-        <div className="bg-muted ftl:w-1/2 ftl:p-8 w-full rounded-lg border p-6">
-          <h2 className="mb-4">In che ambiti viene impiegata maggiormente l&apos;acqua?</h2>
+        <div className="bg-muted ftl:w-3/5 ftl:p-8 w-full rounded-lg border p-6">
+          <h2 className="mb-4 text-2xl">Portata del Po nel tempo</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget nisi enim. Ut sodales urna vitae
             urna mollis, non pretium justo sodales. Nulla ultricies, lorem quis aliquam eleifend, nunc leo dignissim
@@ -66,9 +94,12 @@ export default function Analisi() {
         </div>
       </div>
 
-      <div className="ftl:flex-row ftl:space-x-6 ftl:space-y-0 flex w-full flex-col-reverse items-stretch space-y-6 space-y-reverse space-x-0">
-        <div className="bg-muted ftl:w-1/2 ftl:p-8 w-full rounded-lg border p-6">
-          <h2 className="mb-4">In che ambiti viene impiegata maggiormente l&apos;acqua?</h2>
+      <div
+        id="utilizzi"
+        className="ftl:flex-row ftl:space-x-6 ftl:space-y-0 space-y flex w-full flex-col items-stretch space-y-6 space-x-0"
+      >
+        <div className="bg-muted ftl:w-3/5 ftl:p-8 w-full rounded-lg border p-6">
+          <h2 className="mb-4 text-2xl">In che ambiti viene impiegata maggiormente l&apos;acqua?</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget nisi enim. Ut sodales urna vitae
             urna mollis, non pretium justo sodales. Nulla ultricies, lorem quis aliquam eleifend, nunc leo dignissim
@@ -82,7 +113,7 @@ export default function Analisi() {
           </p>
         </div>
 
-        <div className="bg-muted ftl:w-1/2 flex w-full items-center justify-center overflow-hidden rounded-lg border p-6">
+        <div className="bg-muted ftl:w-2/5 flex w-full items-center justify-center rounded-lg border p-6">
           <ConsumiIdriciChart />
         </div>
       </div>
